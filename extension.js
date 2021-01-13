660734e60b5a8f51a05e4d94c997dd4924957239
Rhone Lachner <fernandclay@gmail.com>
2021-01-13 13:14:22 -0800

    initial sandbox

--- extension.js
+++ extension.js
@@ -21,7 +21,7 @@ function activate(context) {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from be-human-sandbox!');
+		vscode.window.showInformationMessage('YO YO YO YO YO!');
 	});
 
 	context.subscriptions.push(disposable);
